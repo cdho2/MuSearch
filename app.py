b11d1403c9ebda9a6377cd875f52d8d395757d62
Troy E Martin <troydotmartin@gmail.com>
2016-10-27 19:58:45 -0500

    Stuff is working...

--- app.py
+++ app.py
@@ -26,13 +26,31 @@ def index():
     returnThis = "MusicSearch v 0.0.0.01"
     return returnThis
 
-#Keyword endpoint
+@app.route('/search/<keyword>', methods=['GET'])
+def keyword_search(keyword):
+    try:
+        artist = spotify_search_artist(keyword)
+    except (IndexError):
+        artist = spotify_search_song(keyword)
+
+    artist_id = artist['id']
+    playlist = spotify_search_playlist(artist_id)
+
+    artist_name = artist['name']
+    summary = wikipedia_search(artist_name)
+    tweets = tweet_search(artist_name)
+
+    data = {'artist_name':artist_name,'summary':summary,
+    'tweets':tweets, 'playlist':playlist}
+
+    return str(data)
+
 @app.route('/twitter/<keyword>', methods=['GET']) #, methods = ['GET']
 def tweet_search(keyword):
-    #Default max tweets = 10, otherwise it is passed as queryparam 'max'
-    MAX_TWEETS = 10 
+    MAX_TWEETS = 3
     searched_tweets = [status for status in tweepy.Cursor(api.search, q=keyword).items(MAX_TWEETS)]
-    return ' ***** '.join([status.text for status in searched_tweets])
+    tweet_texts = [status.text for status in searched_tweets]
+    return tweet_texts
 
 
 @app.route('/wikipedia/<keyword>', methods=['GET'])
@@ -44,19 +62,24 @@ def wikipedia_search(keyword):
 def spotify_search_artist(keyword):
     results = spotify.search(q='artist:'+keyword, type = 'artist')
     artist_id = results['artists']['items'][0]['id']
-    return artist_id
+    artist_name = results['artists']['items'][0]['name']
+    artist = {'name':artist_name, 'id':artist_id}
+    return artist
 
 @app.route('/spotify/song/<keyword>', methods=['GET'])
 def spotify_search_song(keyword):
     results = spotify.search(q='track:'+keyword, type = 'track')
     artist_id = results['tracks']['items'][0]['artists'][0]['id']
-    return artist_id
+    artist_name = results['tracks']['items'][0]['artists'][0]['name']
+    artist = {'name':artist_name, 'id':artist_id}
+    return artist
 
 @app.route('/spotify/playlist/<artist_id>', methods = ['GET'])
 def spotify_search_playlist(artist_id):
     results = spotify.artist_top_tracks('spotify:artist:' + artist_id)
-    artist_id = results['tracks']['items'][0]['artists'][0]['id']
-    return artist_id
+    songs = [results['tracks'][i]['name'] for i in xrange(min(10,len(results['tracks'])))]
+
+    return songs
 
 
 if __name__ == '__main__':
